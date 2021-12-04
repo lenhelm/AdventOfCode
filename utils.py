7bcf50873772f594096a1241a5f32d1e6a62b446
lenhelm <lennard.helmer@gmail.com>
2021-12-04 18:45:45 +0100

    feat: day4 data load

--- utils.py
+++ utils.py
@@ -4,3 +4,18 @@ def read_data(input_file):
         for line in file.readlines():
             output.append(line.strip())
     return output
+
+
+def read_bingo_data(input_file):
+    output = {}
+    with open(input_file, 'r') as file:
+        bingo_batch = 0
+        for line in file.readlines():
+            if len(line.split(',')) > 5:
+                output['drawn_numbers'] = line.split(',')
+            elif len(line) < 5:
+                bingo_batch +=1
+                output[bingo_batch] = []
+            else:
+                output[bingo_batch].append(line.split())
+        return output
