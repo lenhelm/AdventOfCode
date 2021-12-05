14aa55c706636531406a0ec784a1ddf0251c171d
lenhelm <lennard.helmer@gmail.com>
2021-12-05 20:44:18 +0100

    feat: refactorings and docstings

--- Day4/solution.py
+++ Day4/solution.py
@@ -1,12 +1,31 @@
 import numpy as np
 
 
+def read_bingo_data(input_file):
+    """Read the bingo data"""
+    output = {}
+    with open(input_file, 'r') as file:
+        bingo_batch = 0
+        for line in file.readlines():
+            if len(line.split(',')) > 5:
+                output['drawn_numbers'] = [int(x) for x in line.split(',')]
+            elif len(line) < 5:
+                bingo_batch +=1
+                output[bingo_batch] = []
+            else:
+                output[bingo_batch].append(line.split())
+        return output
+
+
 class Game:
+    """A game with different bingo grids. Evaluate an array of drawn numbers 
+    and receive a dictionary with the first winning grid and the last winning
+    grid."""
 
     def __init__(self, data: dict):
         self.grids = {key: Grid(value) for key, value in data.items()}
     
-    def evaluate(self, drawn_numbers: list):
+    def evaluate(self, drawn_numbers: list) -> dict:
         leaderboard = []
         winner_grids = []
         for number in drawn_numbers:
@@ -21,23 +40,24 @@ class Game:
                         'solution': grid.sum() * number
                     })
         return {
-            'solution_part1': leaderboard[0],
-            'solution_part2': leaderboard[-1]
+            'first_winning_grid': leaderboard[0],
+            'last_winning_grid': leaderboard[-1]
         }
 
 
 class Grid:
+    """A grid of a bingo game"""
 
     def __init__(self, data: list):
         self.grid = np.array(data).astype(int)
     
-    def cross(self, number):
+    def cross(self, number: int):
         self.grid = np.where(self.grid == number, 0, self.grid)
 
     def sum(self):
         return self.grid.sum()
 
-    def eval_win(self):
+    def eval_win(self) -> bool:
         winning_row = self.eval_win_input(self.grid)
         winning_col = self.eval_win_input(self.grid.transpose())
         if any([winning_row, winning_col]):
@@ -45,7 +65,7 @@ class Grid:
         return False 
 
     @staticmethod
-    def eval_win_input(grid: np.array):
+    def eval_win_input(grid: np.array) -> bool:
         for x in grid:
             if x.sum() == 0:
                 return True
--- utils.py
+++ utils.py
@@ -4,18 +4,3 @@ def read_data(input_file):
         for line in file.readlines():
             output.append(line.strip())
     return output
-
-
-def read_bingo_data(input_file):
-    output = {}
-    with open(input_file, 'r') as file:
-        bingo_batch = 0
-        for line in file.readlines():
-            if len(line.split(',')) > 5:
-                output['drawn_numbers'] = [int(x) for x in line.split(',')]
-            elif len(line) < 5:
-                bingo_batch +=1
-                output[bingo_batch] = []
-            else:
-                output[bingo_batch].append(line.split())
-        return output
