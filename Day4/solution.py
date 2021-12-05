e76a3cdbbe4b17aede5016a6beb0e161af58dcae
lenhelm <lennard.helmer@gmail.com>
2021-12-05 19:47:26 +0100

    feat: day4 part1 solution

---
+++ Day4/solution.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+class Game:
+    def __init__(self, data: dict):
+        self.grids = {key: Grid(value) for key, value in data.items()}
+    
+    def evaluate_drawn_numbers(self, drawn_numbers: list):
+        for number in drawn_numbers:
+            for grid_id, grid in self.grids.items():
+                grid.cross(number)
+                if grid.eval_win():
+                    return {
+                        'grid_id': grid_id,
+                        'grid_sum': grid.sum(),
+                        'number': number,
+                        'solution': grid.sum() * number
+                    }
+        return 'No winner yet...'
+
+
+class Grid:
+
+    def __init__(self, data: list):
+        self.grid = np.array(data).astype(int)
+    
+    def cross(self, number):
+        self.grid = np.where(self.grid == number, 0, self.grid)
+
+    def sum(self):
+        return self.grid.sum()
+
+    def eval_win(self):
+        winning_row = self.eval_win_input(self.grid)
+        winning_col = self.eval_win_input(self.grid.transpose())
+        if any([winning_row, winning_col]):
+            return True
+        return False 
+
+    @staticmethod
+    def eval_win_input(grid: np.array):
+        for x in grid:
+            if x.sum() == 0:
+                return True
+        return False
