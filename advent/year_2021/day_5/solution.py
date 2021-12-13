11836a1a861790e01e23e529e425ca0dcee3005b
lenhelm <lennard.helmer@gmail.com>
2021-12-13 16:59:40 +0100

    feat: day_5 algorithm

--- advent/year_2021/day_5/solution.py
+++ advent/year_2021/day_5/solution.py
@@ -12,3 +12,18 @@ def read_data(file_path):
             if any([x1 == x2, y1 == y2]):
                 rows.append(Row(Point(x1, y1), Point(x2, y2)))
     return rows
+
+
+def calculate_intersections(data: list):
+    found_intersections = []
+    count = 0
+    for line_count in range(len(data)):
+        try:
+            intersections = data[line_count].evaluate(data[line_count + 1])
+            if intersections:
+                for intersection in intersections:
+                    if intersection not in found_intersections:
+                        count += 1
+                        #found_intersections.append(intersection)
+        except IndexError:
+            return count
--- test/test_day_5.py
+++ test/test_day_5.py
@@ -1,4 +1,5 @@
-from advent.year_2021.day_5.solution import Point, Row
+from advent.year_2021.day_5.objects import Point, Row
+from advent.year_2021.day_5.solution import calculate_intersections
 
 
 def test_point():
@@ -26,3 +27,10 @@ def test_row_evaluate():
     row2 = Row(Point(1,2), Point(1,4))
 
     assert row1.evaluate(row2) == [Point(1,2), Point(1,3)]
+
+
+def test_calculate_intersections():
+    input = [Row(Point(1,2), Point(1,3)), Row(Point(1,3), Point(1,4)), Row(Point(1,5), Point(1,9))]
+    expected_output = 1
+    assert expected_output == calculate_intersections(input)
+    
