04c8fb4876958a6d58f42ac55dcf421539d50813
lenhelm <lennard.helmer@gmail.com>
2021-12-18 17:33:37 +0100

    test: add test for read_input

--- test/test_day_7.py
+++ test/test_day_7.py
@@ -1,4 +1,12 @@
-from advent.year_2021.day_7.main import first_solution, optimize, calculate_costs
+from pathlib import Path
+
+from advent.year_2021.day_7.main import (
+    first_solution,
+    optimize,
+    calculate_costs,
+    read_input,
+)
+from advent.year_2021 import utils
 
 
 def test_calculate_costs():
@@ -12,3 +20,9 @@ def test_optimize():
     input = [0, 5, 10, 15, 20]
     expected = (10, 30)
     assert optimize(input) == expected
+
+
+def test_read_input():
+    data = read_input("day_7.txt")
+    assert type(data) == list
+    assert all([type(x) == int for x in data])
