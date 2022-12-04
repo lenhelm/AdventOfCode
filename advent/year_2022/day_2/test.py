0f905c452777f10d4c9e83161169540a142a2462
Lennard <you@example.com>
2022-12-04 18:12:55 +0100

    test

--- advent/year_2022/day_2/test.py
+++ advent/year_2022/day_2/test.py
@@ -11,7 +11,7 @@ def test_main():
 def test_count_patterns():
     with open('test_input.txt', 'r') as file:
         text = file.read()
-    expected_output = [('A X', 8,6),('A Y', 8,4), ('A Z', 3,8), ('B X', 1,1),
-                       ('B Y', 5,5), ('B Z', 9,9), ('C X', 14,4), ('C Y', 2,6),
-                       ('C Z', 6,7)]
+    expected_output = [('A X', 8, 6), ('A Y', 8, 4), ('A Z', 3, 8),
+                       ('B X', 1, 1), ('B Y', 5, 5), ('B Z', 9, 9),
+                       ('C X', 14, 4), ('C Y', 2, 6), ('C Z', 6, 7)]
     assert expected_output == count_patterns(text)
